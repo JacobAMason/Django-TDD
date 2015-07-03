4e048068701ac1e93496d5b668d1d58a642a5143
JacobAMason <jm2232@msstate.edu>
2015-07-03 13:29:19 -0500

    Add a URL for creating a new list via POST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 
 __author__ = 'JacobAMason'
 
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -48,19 +49,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
+        )
 
         # Now a new user, Francis, comes along to the site
         # TODO: Adjust model so that items are associated with different lists
         # TODO: Add unique URLs for each list
-        # TODO: Add a URL for creating a new list via POST
         # TODO: Add URLs for adding a new item to an existing list via POST
 
         ## We use a new browser session to make sure that no information of
